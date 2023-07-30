d67bcd3665d105e6253a7197e85a3e39c9aa2389
NicolasRuan <nicolasruan@pop-os.localdomain>
2023-07-30 17:40:58 -0300

    deploy test 1

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello world" });
+  res.send({ message: "Hello world" });
 });
 
 app.route("/users").post(UserController.create).get(UserController.listAll);
